b7fa4347914e5494c2d5d2ce4c4fc29e8e6a6390
Joice Maciel <joicemacielmartins@gmail.com>
2023-08-30 15:21:13 -0300

    criada variáveis com inputs do formulário

---
+++ javascript/seus-dados.js
@@ -0,0 +1,19 @@
+//dados pessoais
+let $form = document.querySelector(".formulario");
+let $nomeInput = document.querySelector("#nome");
+let $sobrenomeInput = document.querySelector("#sobrenome");
+let $RGInput = document.querySelector("#rg");
+let $CPF1Input = document.querySelector("#cpf1");
+let $CPF2Iput = document.querySelector("#cpf2");
+
+//dados de endereço
+let $ruaInput = document.querySelector("#rua");
+let $numeroInput = document.querySelector("#numero");
+let $bairroInput = document.querySelector("#bairro");
+let $cidadeInput = document.querySelector("#cidade");
+let $CEP1Input = document.querySelector("#cep1");
+let $CEP2Input = document.querySelector("#cep2");
+
+//dados de login
+let $emailInput = document.querySelector("#email");
+let $botaoEnviar = document.querySelector("#botao-enviar");
